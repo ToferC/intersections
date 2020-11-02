16625a5f3b6cb173cf8c9846ed776a489c3537ab
toferc <cgeist7@gmail.com>
2020-11-02 14:55:50 -0500

    remove trim psql migration

--- migrations/2020-11-02-174605_trim_spaces/down.sql
+++
@@ -1,5 +0,0 @@
--- This file should undo anything in `up.sql`
-
-DELETE from nodes
-WHERE node_name IN (
-    ' black');
--- migrations/2020-11-02-174605_trim_spaces/up.sql
+++
@@ -1,6 +0,0 @@
--- Your SQL goes here
-
-UPDATE nodes
-SET node_name = TRIM (node_name)
-ON CONFLICT (node_name)
-    DO NOTHING;
